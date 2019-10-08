a6f13efd87833259e6b5b17f292b46943f512936
yohannatoro <yoha_duran156@hotmail.com>
2019-10-08 17:44:36 -0500

    finish

--- src/main/java/edu/escuelaing/demo/MainApplication.java
+++ src/main/java/edu/escuelaing/demo/MainApplication.java
@@ -3,11 +3,20 @@ package edu.escuelaing.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import edu.escuelaing.demo.config.*;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"edu.escuelaing.demo"})
 public class MainApplication {
+    public FilterRegistrationBean jwtFilter()
+	{
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter( new JwtFilter() );
+		registrationBean.addUrlPatterns( "/api/*" );
 
+		return registrationBean;
+	}
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
--- src/main/java/edu/escuelaing/demo/controller/UserController.java
+++ src/main/java/edu/escuelaing/demo/controller/UserController.java
@@ -1,6 +1,9 @@
 package edu.escuelaing.demo.controller;
 
+import java.util.Date;
 import java.util.List;
+
+import javax.servlet.ServletException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import edu.escuelaing.demo.model.User;
 import edu.escuelaing.demo.service.UserServices;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RestController
 @RequestMapping(value="/users")
@@ -35,6 +40,63 @@ public class UserController{
             return new ResponseEntity<>(e.getLocalizedMessage(),HttpStatus.NOT_FOUND);
         }
 
+    }
+    @RequestMapping( value = "/login", method = RequestMethod.POST )
+    public Token login( @RequestBody User login )
+        throws ServletException
+    {
+
+        String jwtToken = "";
+
+        if ( login.getName() == null || login.getPassword() == null )
+        {
+            throw new ServletException( "Please fill in username and password" );
+        }
+
+        String username = login.getName();
+        String password = login.getPassword();
+
+        //TODO implement logic to verify user credentials
+        User user = userServices.getUserById(login.getId());
+
+        if ( user == null )
+        {
+            throw new ServletException( "User username not found." );
+        }
+
+        String pwd = user.getPassword();
+
+        if ( !password.equals( pwd ) )
+        {
+            throw new ServletException( "Invalid login. Please check your name and password." );
+        }
+        //
+        jwtToken = Jwts.builder().setSubject( username ).claim( "roles", "user" ).setIssuedAt( new Date() ).signWith(
+            SignatureAlgorithm.HS256, "secretkey" ).compact();
+
+        return new Token( jwtToken );
+    }
+    public class Token
+    {
+
+        String accessToken;
+
+
+        public Token( String accessToken )
+        {
+            this.accessToken = accessToken;
+        }
+
+
+        public String getAccessToken()
+        {
+            return accessToken;
+        }
+
+        public void setAccessToken( String access_token )
+        {
+            this.accessToken = access_token;
+        }
     }
     
 
